2909a12e605bd63e6c060ce28b22dfcd8048e828
felix <newgpp@hotmail.com>
2026-01-02 08:41:34 +0800

    feat: 转发url更新

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8081',
+        target: 'http://localhost:10052',
         changeOrigin: true
       }
     }
